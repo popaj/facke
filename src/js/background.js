7e663d80175e6ef962863ab8ac3e6c099b17a08c
popaj <popaj@fatmirs-mbp.lan>
2024-01-23 17:56:40 +0100

    refactor

--- src/js/background.js
+++ src/js/background.js
@@ -34,6 +34,18 @@ async function saveToLocalStorage(host, data) {
     });
 }
 
+async function getAuthorsFromMasterData(host) {
+    try {
+        const response = await fetch(ABSOLUTE_PATH_PREFIX + hostToAuthorsMap[host]);
+        const authors = await response.json();
+        await saveToLocalStorage(host, authors);
+        return authors;
+    } catch (error) {
+        console.error("Error in getAuthorsFromMasterData:", error);
+        return [];
+    }
+}
+
 function removeWWW(host) {
     return host.startsWith("www.") ? host.substring(4, host.length) : host;
 }
@@ -46,75 +58,35 @@ function transferToContent(action, payload) {
 }
 
 browser.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-    DEBUG && console.log("... receiving ...");
-
     if (message.action === "addAuthor" && message.author) {
-
-        DEBUG && console.group("addAuthor");
-        DEBUG && console.log("> message", message);
-
-        const host = removeWWW(message.host);
-        let localStorageAuthors = await getFromLocalStorage(host);
-        localStorageAuthors = JSON.parse(localStorageAuthors[host]);
-        DEBUG && console.log("localStorageAuthors", localStorageAuthors);
-        DEBUG && console.log("localStorageAuthors.length", localStorageAuthors.length);
-        const newAuthor = new AuthorBuilder()
-            .setFirstName(message.author)
-            .setLastName(message.author)
-            .build();
-        DEBUG && console.log("Before authorUpdated", localStorageAuthors);
-        DEBUG && console.log("Before authorUpdated.length", localStorageAuthors.length);
-        localStorageAuthors.push(newAuthor);
-        DEBUG && console.log("After authorUpdated", localStorageAuthors);
-        DEBUG && console.log("After authorUpdated.length", localStorageAuthors.length);
-        DEBUG && console.groupEnd();
-
-        await saveToLocalStorage(host, localStorageAuthors);
-        transferToContent("addAuthor", localStorageAuthors);
+        try {
+            const host = removeWWW(message.host);
+            let authors = await getFromLocalStorage(host);
+            authors = JSON.parse(authors[host]);
+            const newAuthor = new AuthorBuilder()
+                .setFirstName(message.author)
+                .setLastName(message.author)
+                .build();
+            authors.push(newAuthor);
+            await saveToLocalStorage(host, authors);
+            transferToContent("addAuthor", authors);
+        } catch (error) {
+            console.error("Error saving new author:", error);
+            transferToContent("addAuthor", []);
+        }
     } else if (message.action === "getAuthors") {
-
-        DEBUG && console.group("getAuthors");
-        DEBUG && console.log("> message", message);
-        DEBUG && console.groupEnd();
-
         let host = removeWWW(message.host);
         try {
             const localStorageAuthors = await getFromLocalStorage(host);
-            const payload = localStorageAuthors[host];
-
-            let globalData;
-            if (payload && payload.length > 0) {
-
-                DEBUG && console.group("from localStorage");
-                DEBUG && console.log("> localStorage", localStorageAuthors);
-                DEBUG && console.log("> payload", payload);
-                DEBUG && console.groupEnd();
-
-                transferToContent("getAuthors", payload);
-            } else {
-                DEBUG && console.group("from file");
-
-                const jsonFileName = hostToAuthorsMap[host];
-                const jsonFileURL = browser.runtime.getURL(ABSOLUTE_PATH_PREFIX + SLASH + jsonFileName);
-
-                DEBUG && console.log("> jsonFileName", jsonFileName);
-                DEBUG && console.log("> jsonFileURL", jsonFileURL);
-
-                await fetch(jsonFileURL)
-                    .then(response => response.json())
-                    .then(data => {
-                        // Transfer to Content
-                        globalData = data;
-                        DEBUG && console.log('data', data);
-                        transferToContent("getAuthors", data);
-                    })
-                    .catch(error => console.error('Error reading authors.json:', error));
-                await saveToLocalStorage(host, globalData);
-                DEBUG && console.groupEnd();
+            let authors = localStorageAuthors[host];
+            if (!authors) {
+                authors = await getAuthorsFromMasterData(host);
             }
-
+            sendResponse({authors});
+            transferToContent("getAuthors", authors)
         } catch (error) {
             console.error("Error retrieving data:", error);
+            transferToContent("getAuthors", []);
         }
     }
 });
